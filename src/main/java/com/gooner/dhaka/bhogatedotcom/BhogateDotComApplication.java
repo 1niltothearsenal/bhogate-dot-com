eef5badfa39c095aa0e90c24dd68514befa68be5
1niltothearsenal <dhakal.saurae@gmail.com>
2018-10-02 17:57:09 -0500

    Initial Push

---
+++ src/main/java/com/gooner/dhaka/bhogatedotcom/BhogateDotComApplication.java
@@ -0,0 +1,12 @@
+package com.gooner.dhaka.bhogatedotcom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BhogateDotComApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BhogateDotComApplication.class, args);
+    }
+}
